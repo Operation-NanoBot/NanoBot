68142d1a081e0576234c1ded070d04d3c256dd9f
TheNandre <NathanEAAndre@gmail.com>
2017-08-26 00:01:43 -0500

    Add SideScroll Direction States

---
+++ NanoBot_Demo/Assets/Code/NanoBot/SideScroll/NB_SS_Direction_State.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public abstract class NB_SS_Direction_State
+{
+    //Functions//
+
+    //Constructor
+    protected NB_SS_Direction_State()
+    {
+
+    }
+
+    public abstract void Update(NbSidescrollController NB);
+}
